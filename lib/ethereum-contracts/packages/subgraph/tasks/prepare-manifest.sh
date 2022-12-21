7447f903db2411c26297a8c7b62c7c4e02cd7d65
danny ba <dannba0@gmail.com>
2022-12-21 12:43:47 +0100

    push

--- lib/ethereum-contracts/packages/subgraph/tasks/prepare-manifest.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# args
-# $1 = the network
-
-mustache="../../node_modules/mustache/bin/mustache"
-npx mustache config/$1.json subgraph.template.yaml > subgraph.yaml
-npx mustache config/$1.json src/addresses.template.ts > src/addresses.ts
--- src/rNFT_contracts/ReNFT.sol
+++ src/rNFT_contracts/ReNFT.sol
@@ -8,7 +8,6 @@ import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Receiver.sol";
 import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
-
 import "../interfaces/IReNFT.sol";
 import "../interfaces/IResolver.sol";
 
