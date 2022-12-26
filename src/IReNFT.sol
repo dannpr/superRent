7f0f30c650d8f9e44216141c6cac5057ce492c2c
danny ba <dannba0@gmail.com>
2022-12-26 21:37:16 +0100

    new

--- src/IReNFT.sol
+++ src/IReNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.16;
+pragma solidity =0.8.6;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
